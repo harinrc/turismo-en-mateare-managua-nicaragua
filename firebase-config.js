f46e1b0a5c11d5908069e87108ec2220593edf04
Harinto José Rodríguez Cruz <djhjrc96@gmail.com>
2026-07-06 01:21:31 -0600

    firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,17 @@
-// Reemplaza estos valores con la configuracion de tu proyecto Firebase.
-// Puedes obtenerlos en Firebase Console > Project settings > General > Your apps > SDK setup.
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVGjiN3nEaqr4rpfYKDhvUtGWUI6JVxWw",
+  authDomain: "turismo-en-mateare.firebaseapp.com",
+  projectId: "turismo-en-mateare",
+  storageBucket: "turismo-en-mateare.firebasestorage.app",
+  messagingSenderId: "853770360232",
+  appId: "1:853770360232:web:b5165b7b76a41a6f39799d"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
